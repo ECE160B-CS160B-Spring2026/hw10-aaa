ba2bdc9908aa86c83ff59c2994a2796054befe16
aublaylock <austin.blaylock.gsq@gmail.com>
2026-04-18 17:34:34 -0400

    Attempt2

--- question2/q2.c
+++ question2/q2.c
@@ -113,7 +113,7 @@ void treeprint(struct tnode *p)
     if (p != NULL)
     {
         treeprint(p->left);
-        printf("%4d %s\n", p->count, p->word);
+        printf("%d %s\n", p->count, p->word);
         treeprint(p->right);
     }
 }
